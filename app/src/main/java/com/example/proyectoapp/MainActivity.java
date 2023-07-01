f310e01d713477d7cccee52ad8a86adb9fc9beb8
alejandro <alejoio310@gmail.com>
2023-07-01 17:52:29 -0500

    avance

--- app/src/main/java/com/example/proyectoapp/MainActivity.java
+++ app/src/main/java/com/example/proyectoapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.proyectoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar practica =findViewById(R.id.myToolbar);
+        setSupportActionBar(practica);
+        getSupportActionBar().setTitle("");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mymenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.icono1){
+            Toast.makeText(getApplicationContext(), "Icono 1", Toast.LENGTH_SHORT).show();
+        }
+        if (id == R.id.icono2){
+            Toast.makeText(getApplicationContext(), "Icono 2", Toast.LENGTH_SHORT).show();
+        }
+        if (id == R.id.icono3){
+            Toast.makeText(getApplicationContext(), "Icono 3", Toast.LENGTH_SHORT).show();
+        }
+        if (id == R.id.icono4){
+            finish();
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/example/proyectoapp/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.proyectoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Button  ingresar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        }
+
+        public void funcion(){
+
+
+
+    }
+}
